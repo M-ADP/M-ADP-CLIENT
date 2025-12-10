4537a710ea6569d4ecb846a331e6c9308b20cba4
zuu3 <ojh27467242@gmail.com>
2025-12-10 19:55:24 +0900

    feat(MADP-47) :: Button 공용 컴포넌트 타입 분리

--- src/components/ui/Button/ui.tsx
+++ src/components/ui/Button/ui.tsx
@@ -1,14 +1,7 @@
 'use client';
 
 import { StyledButton } from './style';
-
-type ButtonProps = {
-  variant?: 'confirm' | 'cancel';
-  onClick?: () => void;
-  disabled?: boolean;
-  className?: string;
-  children?: React.ReactNode;
-};
+import type { ButtonProps } from '@/types/button';
 
 export default function Button({ variant = 'confirm', onClick, disabled = false, className, children }: ButtonProps) {
   const defaultText = variant === 'confirm' ? '확인' : '취소';
